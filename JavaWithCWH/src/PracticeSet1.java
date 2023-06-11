bda7e9fe40d22f1c32f20ad4fcfb5a02874723e1
simran0963 <simranmehta0963@gmail.com>
2023-06-11 17:23:50 +0530

    arrays

---
+++ JavaWithCWH/src/PracticeSet1.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+public class PracticeSet1 {
+    public static void main(String[] args) {
+        // Qs1 :
+//        int a=2;
+//        int b=2;
+//        int c=4;
+//        System.out.println(a+b+c);
+
+        //qs2.
+//        float sub1 = 45;
+//        float sub2 = 95;
+//        float sub3 = 48;
+//        float cgpa = (sub1+sub2+sub3)/30;
+//        System.out.println(cgpa);
+
+        //qs3.
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("What is your name");
+//        String name = sc.nextLine();
+//        System.out.println("Helo " + name);
+
+        //qs4
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter the value in kilometers : ");
+//        float km = sc.nextFloat();
+//        float m = km*1000;
+//        System.out.println(km + " kms = " + m + "m");
+
+        //qs5.
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value : ");
+        System.out.println(sc.hasNextInt());
+    }
+}
---
+++ JavaWithCWH/src/cwh_12_ps2.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.Scanner;
+public class cwh_12_ps2 {
+    public static void main(String[] args) {
+        /*float a = 7 / 4.0f * 9 / 2.0f;
+        System.out.println(a);*/
+
+        /*char grade = 'B';
+        grade = (char)(grade + 8);
+        System.out.println(grade);
+        grade = (char)(grade - 8);
+        System.out.println(grade);*/
+
+        /*int a = 10;
+        Scanner sc = new Scanner(System.in);
+        int b = sc.nextInt();
+        if(a>b) System.out.println("true");
+        else if (a==b) System.out.println("equal");
+        else System.out.println("false");*/
+
+        /*((v*v - u*u)/(2*a*s))*/
+    }
+}
